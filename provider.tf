f29712ca2d6a6d2c038dcca60df74c869ef12d93
ashikjen200 <35518899+ashikjen200@users.noreply.github.com>
2024-03-04 10:17:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
 provider "tfe" {
   hostname = app.terraform.io
   token    = var.token_id

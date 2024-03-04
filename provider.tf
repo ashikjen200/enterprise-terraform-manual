807f81145602e20300fb22a0fcc0ac9686f0054e
ashikjen200 <35518899+ashikjen200@users.noreply.github.com>
2024-03-04 10:15:19 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,11 @@ terraform {
     }
   }
 }
-
+provider "tfe" {
+  hostname = app.terraform.io
+  token    = var.token_id
+  version  = "~> 0.52.0"
+}
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key

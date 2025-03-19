031a70bbc569e20499512b2af54c5d74fa1212ae
ashikjen200 <35518899+ashikjen200@users.noreply.github.com>
2025-03-19 11:05:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,34 @@ resource "aws_instance" "public_instance" {
    Name = var.name_tag,
  }
 }
+
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "my-public-bucket-example-12345"
+  acl    = "public-read"  # This grants public read access to the bucket
+
+  tags = {
+    Name = "Public Bucket"
+  }
+}
+
+resource "aws_s3_bucket_object" "public_object" {
+  bucket = aws_s3_bucket.public_bucket.bucket
+  key    = "public_file.txt"
+  content = "This is a publicly accessible file."
+}
+
+resource "aws_s3_bucket_policy" "public_bucket_policy" {
+  bucket = aws_s3_bucket.public_bucket.bucket
+
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.public_bucket.arn}/*"
+      }
+    ]
+  })
+}
